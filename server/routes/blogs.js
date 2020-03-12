16f00021d3e1f935ff942a216293660d8156e889
Jesus Vega <jesusvega00@gmail.com>
2020-03-12 13:11:02 -0700

    commit

--- server/routes/blogs.js
+++ server/routes/blogs.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Blog = require('../models/Blog');
+const User = require('../models/User');
 
 router.get('/', (req, res) => {
     Blog
@@ -12,11 +13,12 @@ router.get('/', (req, res) => {
 
 router.get('/featured', (req, res) => {
     Blog
-    .where({featured: true})
-    .then(blogs => res.status(200).json(blogs))
-    .catch(err => res.status(500).send(err))
+        .find()
+        .where('featured').equals(true)
+        .then(featured => {
+            res.status(200).json(featured);
+        })
 });
-
 
 router.get('/:id', (req, res) => {
     Blog
@@ -26,21 +28,44 @@ router.get('/:id', (req, res) => {
         });
 });
 
+// router.post('/', (req, res) => {
+//     const newBlog = new Blog({
+//         title: req.body.title,
+//         article: req.body.article,
+//         published: req.body.published,
+//         feature: req.body.feature,
+//         author: req.body.author,
+//     });
+//     newBlog.save()
+//     .then(blogs => {
+//         res.status(201).send("saved to database");
+//     })
+//     .catch(err =>{
+//         res.status(400).send("cant save to database")
+//     });
+// });
+
+
 router.post('/', (req, res) => {
-    const newBlog = new Blog({
-        title: req.body.title,
-        article: req.body.article,
-        published: req.body.published,
-        feature: req.body.feature,
-        author: req.body.author,
-    });
-    newBlog.save()
-    .then(blogs => {
-        res.status(201).send("saved to database");
-    })
-    .catch(err =>{
-        res.status(400).send("cant save to database")
-    });
+    let dbUser = null;
+
+    User
+        .findById(req.body.author)
+        .then(user => {
+            dbUser = user;
+
+            const newBlog = new Blog(req.body);
+
+            newBlog.author = user._id;
+
+            return newBlog.save();
+        })
+        .then(blog => {
+            dbUser.blogs.push(blog);
+
+            dbUser.save().then(() => res.status(201).json(blog));
+        })
+
 });
 
 router.put('/:id', (req, res) => {
@@ -72,5 +97,15 @@ router.delete('/:id', (req, res) => {
             }
         });
 });
+
+// router.delete('/:id', (req, res) => {
+//     Blog
+//         .findByIdAndDelete(req.params.id)
+//         .then(blog => {
+//             if(!blog) return res.status(404).send('no blog found');
+//             res.status(200).json(blog);
+//         })
+//         .catch(err => console.log(err))
+// })
 
 module.exports = router;
